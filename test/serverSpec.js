a49be85cd2cc0c0278b003e19de1b9528debf870
nadaa <nada.saif7@gmail.com>
2018-08-15 21:31:12 +0300

    test server get & post

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -1,1 +1,38 @@
-  // TODO: Your code here
+  // test get request
+  //const expect=require('mocha').expect;
+
+const chai=require('chai')
+const expect=chai.expect;
+const chai_http=require('chai-http');
+chai.use(chai_http);
+const app=require('../server/index');
+
+
+describe('test get and post requests',function(done){
+	it('pass with sucessful post request',function(){
+	   chai.request(app)
+	  .post('/cats')
+	  .send({ catName:'meme',
+	   ownerEmail:'sara@gmail.com',
+	   imageUrl:'image',
+	   adoptionMessage:'I like this cat'})
+	  .end(function (err, res) {
+	     expect(err).to.be.null;
+	     expect(res).to.have.status(200);
+	     done();
+	  });
+	})
+
+	it('pass with sucessful get request',function(){
+		chai.request(app)
+		  .get('/cats')
+		  .end(function (err, res) {
+		     expect(err).to.be.null;
+		     expect(res).to.have.status(200);
+		     done();
+		  });
+	})
+})
+
+
+
